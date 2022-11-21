77879994d9e0ecbf19293b84c25d09e2642c71c7
FranSabt <hsabatino1@gmail.com>
2022-11-21 17:12:55 -0400

    modelos temporales

---
+++ src/models/reviews.js
@@ -0,0 +1,8 @@
+let  reviews =
+    {
+        date: "", //* date,
+        comment: "", //* String
+        rating: "", //* INT
+        wineID: "", //* id foreana
+        userID: "", //* id foreana
+    }
---
+++ src/models/wines.js
@@ -0,0 +1,19 @@
+let wines = [
+    {
+        name: "", //*string
+        brand: "", //*string
+        region: "", //*string
+        description: "",//*string
+        type: "", //!enum? //buscar tipos!!
+        body: "",//*string
+        cropYear: "",//* INT
+        origin: "", //* string
+        volume: "", //* Number
+        alcoholVolume: "", //* NUMBER
+        rating: "", //* NUMBER
+        images: "", //TODO Array de string?
+        strain: "", //* string
+        stock: "", //* INT
+        price: "", //* NUMBER
+    }
+]
