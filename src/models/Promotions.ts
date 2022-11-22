d6775bf0d2bada082486da2ee6a5e7c48a67b662
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-11-22 01:06:19 -0300

    cart and promotions models

--- src/models/Promotions.ts
+++ src/models/Promotions.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const PromotionSchema = new Schema({
+    wine_id: {type: mongoose.Types.ObjectId, ref:"Wine", required: true},
     name: { type: String, trim: true },
     description: String,
-    discount_rate: {type: Number, default: 1},
+    discount_rate: {type: Number, default: 0, min:1, max:99},
     startDate: {type: Date, required: true},
     endDate: {type: Date, required: true}
 })
