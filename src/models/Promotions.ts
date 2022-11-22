edb2e9a2c3b07391d00718c99a6cf3b74f187ebe
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-11-21 21:06:35 -0300

    promotions beta

--- src/models/Promotions.ts
+++ src/models/Promotions.ts
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const PromotionSchema = new Schema({
-    name: String,
+    name: { type: String, trim: true },
     description: String,
-    discount_rate: Number,
-    startDate: Date,
-    endDate: Date
+    discount_rate: {type: Number, default: 1},
+    startDate: {type: Date, required: true},
+    endDate: {type: Date, required: true}
 })
 
 const PromotionModel = mongoose.model('Promotion', PromotionSchema);
