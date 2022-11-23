292971e9ce81e7cd023b11b6d346afc118e84d19
eub1 <ebonforte@gmail.com>
2022-11-23 16:54:18 -0300

    agrego informacion al Readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # wine-supply-back
 
 Nombres de archivos en "PascalCase", excepcion app.ts y index.ts.
+
+# SCRIPTS
+
+npm run dev: para usar nodemon, mira los cambios en el archivo index.ts
+
+npm run build: para hacer el build, que va a transpilar todo a js
+
+npm start: que corre node en index.js (que esta en el build), se puede usar una vez hecho el build
+
+el archivo .env debe estar en la carpeta principal del repo, donde esta el README
--- src/routes/winesRoutes/WinesFilters.ts
+++ src/routes/winesRoutes/WinesFilters.ts
@@ -9,17 +9,20 @@ const router = Router()
 
 
 router.get("/", async (req: Request, res: Response) => {
+
+  const querys = req.query; 
+  if(!querys || querys === undefined) {throw new Error("Please select a filter") };
+
   try {
-    const querys = req.query; 
-    if(!querys || querys === undefined) {throw new Error("Please select a filter") };
-    
+  
     const filteredWines = await Wine.find(querys);
     
-    console.log("filteredWines:", filteredWines);
+    // console.log("filteredWines:", filteredWines);
 
     res.send(filteredWines);
     
   } catch (error: any) {
+
     console.log("error en la ruta /wines/filters")
     res.status(400).send(error.message)
   }
