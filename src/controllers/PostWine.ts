5726d47adcc96c7afbc1f3b3d1eb80fe62a3a92c
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-11-23 01:59:22 -0300

    borrar comments

--- src/controllers/PostWine.ts
+++ src/controllers/PostWine.ts
@@ -1,6 +1,40 @@
-//
+import mongoose from "mongoose"
+import Wine from "../models/Wine"
+// import Review from "../models/Review";
 
-const postWine = async()=> {}
+
+
+
+const postWine = async (name: string, brand: string, description: string, type: string, body: string, cropYear: number, origin: string, zone: string, volume: number, alcoholVolume: number, rating: number, images: Array<string>, strain: string, quantity: number, price: number) => {
+
+    try {
+        const newWine = new Wine(
+            {
+                name,
+                brand,
+                description,
+                type,
+                body,
+                cropYear,
+                origin,
+                zone,
+                volume,
+                alcoholVolume,
+                rating,
+                images,
+                strain,
+                quantity,
+                price,
+                review_id: new mongoose.Types.ObjectId
+            })
+        const createdWine = await newWine.save()
+
+        return createdWine
+
+    } catch (error: any) {
+        console.log(error);
+    }
+};
 
 
 
--- src/routes/adminRoutes/PostWine.ts
+++ src/routes/adminRoutes/PostWine.ts
@@ -1,14 +1,56 @@
-import { Router } from "express";
+import { Router, Response, Request } from "express";
 import postWine from "../../controllers/PostWine"
 
 const router = Router()
 
 //* /admin/post
 
+/*
 router.get("/", (req, res) => {
   try {
       console.log("soy la ruta /admin/post")
       res.send("checking")
+  } catch (error: any) {
+      throw new Error( error );
+  }
+})
+dejo la ruta de prueba momentaneamente
+*/ 
+
+// interface WineRequest<T> extends Request {
+//         name: string,
+//         brand: string,
+//         description: string,
+//         type: string,
+//         body: string,
+//         cropYear: number,
+//         origin: string,
+//         zone: string,
+//         volume: number,
+//         alcoholVolume: number,
+//         rating: number,
+//         images: Array<string>,
+//         strain: string,
+//         review_id: mongoose.Types.ObjectId
+//       }
+
+router.post("/", async (req: Request, res: Response) => {
+
+  let { name, brand, description, type, body, cropYear, origin, zone, volume, alcoholVolume, rating, images, strain, quantity, price } =
+  req.body;
+
+const validate =
+  name && brand && description && type && body && cropYear && origin && zone && volume && alcoholVolume && rating && images && strain && quantity && price
+    ? true
+    : false;
+
+if (!validate) return res.status(400).send(`Missing data!`);
+
+  try {
+      const newWine = postWine(name, brand, description, type, body, cropYear, origin, zone, volume, alcoholVolume, rating, images, strain, quantity, price)
+
+      res.status(200).json(newWine)
+
   } catch (error: any) {
       throw new Error( error );
   }
