369b1f35a6428cc888d9a64cf95195c417a4bbac
Mica Gomez <emivigil12@gmail.com>
2022-11-23 19:42:54 -0300

    Ruta-Controller RecommendedWines

--- src/routes/Index.ts
+++ src/routes/Index.ts
@@ -6,17 +6,15 @@ import WinesFilters from './winesRoutes/WinesFilters';
 import RecommendedWines from './winesRoutes/RecommendedWines';
 import PostWine from './adminRoutes/PostWine'
 
-
+ 
  const router = Router()
-
+ 
 
 router.use('/wines', AllWines);
 router.use('/wines/filters', WinesFilters);
 router.use('/wines/recomendados', RecommendedWines);
-
-router.use('/wine/:id', WineId);
-
 router.use('/admin/post', PostWine);
+router.use('/wine/:id', WineId);
 
 
 export default router;
