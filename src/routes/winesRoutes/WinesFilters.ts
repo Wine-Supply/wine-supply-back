c00ac62874cebed35905b741676b02edd8f17c36
eub1 <ebonforte@gmail.com>
2022-11-23 18:40:05 -0300

    validate emptyquery

--- src/routes/winesRoutes/WinesFilters.ts
+++ src/routes/winesRoutes/WinesFilters.ts
@@ -1,29 +1,27 @@
 import { Router, Request, Response} from "express";
-import Wine from "../../models/Wine"
+import Wine from "../../models/Wine";
+import checkEmptyQuery from "../../controllers/FilterWines"
 
 const router = Router()
 
 //* /wines/filters
 
-// req.query = name // brand // type // body // cropYear // origin // zone // volume // strain
-
-
 router.get("/", async (req: Request, res: Response) => {
 
   const querys = req.query; 
-  if(!querys || querys === undefined) {throw new Error("Please select a filter") };
-
+ 
   try {
-  
-    const filteredWines = await Wine.find(querys);
-    
-    // console.log("filteredWines:", filteredWines);
 
+   const noEmptyQuerys = checkEmptyQuery(querys);
+ 
+   if (noEmptyQuerys) {
+    const filteredWines = await Wine.find(querys);      
+    // console.log("filteredWines:", filteredWines);
     res.send(filteredWines);
-    
+   }
+
   } catch (error: any) {
 
-    console.log("error en la ruta /wines/filters")
     res.status(400).send(error.message)
   }
 });
