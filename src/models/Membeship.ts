f1c3fe3a455d5c567ebd2c421b7eaec06c7b0005
eub1 <ebonforte@gmail.com>
2022-11-28 19:47:02 -0300

    add timestamps, trim y cambio price type x num

--- src/models/Membeship.ts
+++ src/models/Membeship.ts
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const MembershipSchema = new Schema ({
-    name: { type: String, required: true },
-    price: { type: Boolean, required: true },
+    name: { type: String, required: true, trim: true },
+    price: { type: Number, required: true },
     isPremium: { type: Boolean, required: true}
-})
+}, { timestamps: true })
 
 const MembershipModel = mongoose.model('Membership', MembershipSchema);
 export default MembershipModel;
