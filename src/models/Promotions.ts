e00415dd23bc18b351933917683f0d853fc80914
eub1 <ebonforte@gmail.com>
2022-11-28 19:48:00 -0300

    agrego validaciones, isActive y timestamps

--- src/models/Promotions.ts
+++ src/models/Promotions.ts
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 
 const PromotionSchema = new Schema({
     wine_id: {type: mongoose.Types.ObjectId, ref:"Wine", required: true},
-    name: { type: String, trim: true },
-    description: String,
-    discount_rate: {type: Number, default: 0, min:1, max:99},
+    name: { type: String, maxLength: 25, required: true, trim: true },
+    description: { type: String, maxLength: 200, required: true, trim: true },
+    discount_rate: {type: Number, default: 0, min:0, max:99},
     startDate: {type: Date, required: true},
-    endDate: {type: Date, required: true}
-})
+    endDate: {type: Date, required: true},
+    isActive: {type: Boolean, default: true},
+}, { timestamps: true })
 
 const PromotionModel = mongoose.model('Promotion', PromotionSchema);
 export default PromotionModel;
