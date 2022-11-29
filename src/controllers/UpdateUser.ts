44fa3a76bd82b119a707c986619e35c5f3bd05d0
eub1 <ebonforte@gmail.com>
2022-11-29 19:29:18 -0300

    updateUser controller

--- src/controllers/UpdateUser.ts
+++ src/controllers/UpdateUser.ts
@@ -2,10 +2,20 @@ import User from "../models/User"
 
 const updateUser = async (user:any, body:any) => {
 
-  const {isMember} = body.membership_id[0]
-  if( isMember ) {
+  const updatedUser = user
+  // console.log(body);
 
-   user.membership_id[0].isMember = true 
+  const {isMember} = body.membership_id[0]
+  
+  // console.log(body.membership_id[0]);
+  
+  if(Object.keys(body.membership_id).length) {
+
+    if(user.membership_id.length === 0){
+
+   user.membership_id.push({isMember: true})
+   }
+   user.membership_id[0].isMember = isMember
 
    const updatedUser = await user.save()
 
@@ -17,9 +27,8 @@ const updateUser = async (user:any, body:any) => {
     user[property]= body[property]
   }
 
-  const updatedUser = await user.save()
 
-  return updatedUser
+  return updatedUser;
 
 };
 
