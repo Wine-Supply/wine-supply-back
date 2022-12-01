a76a645c6d6c39eecd10ea9fa96c3eedb4cb6dcf
eub1 <ebonforte@gmail.com>
2022-12-01 02:40:30 -0300

    updateUser route + postMembership

---
+++ src/controllers/PostMembership.ts
@@ -0,0 +1,29 @@
+
+const postMembership = async (user:any, body:any) => {
+
+  const updatedUser = user
+  //  console.log(body);
+  
+  if(body.membership_id) {
+    const {isMember} = body.membership_id[0]
+  
+    // console.log(body.membership_id[0]);
+
+    if(user.membership_id.length === 0){
+
+   user.membership_id.push({isMember: true})
+   }
+   user.membership_id[0].isMember = isMember
+
+   await user.save()
+
+   return updatedUser
+
+  }
+
+
+};
+
+
+
+export default postMembership;
