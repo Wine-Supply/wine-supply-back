3fe217598722600757a4462b2e11d9776d079f15
eub1 <ebonforte@gmail.com>
2022-12-01 13:59:24 -0300

    no usamos tabla address

--- src/models/Adress.ts
+++ src/models/Adress.ts
@@ -1,20 +1,20 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const AddressSchema = new Schema({
-    ifDefault: { type: Boolean, required: true },
-    country: { type: String, required: true , trim: true },
-    region: { type: String, required: true, trim: true },
-    city: { type: String, required: true, trim: true },
-    postalCode: { type: String, required: true, trim: true },
-    addressLine1: { type: String, required: true, trim: true },
-    addressLine2: { type: String, required: false, trim: true },
-    addressNumber: { type: String, required: true, trim: true },
-    floor: { type: Number, required: false, trim: true },
-    flatNumber: { type: Number, required: false, trim: true },
-    isActive: {type: Boolean, default: true},
-}, { timestamps: true })
+// const AddressSchema = new Schema({
+//     ifDefault: { type: Boolean, required: true },
+//     country: { type: String, required: true , trim: true },
+//     region: { type: String, required: true, trim: true },
+//     city: { type: String, required: true, trim: true },
+//     postalCode: { type: String, required: true, trim: true },
+//     addressLine1: { type: String, required: true, trim: true },
+//     addressLine2: { type: String, required: false, trim: true },
+//     addressNumber: { type: String, required: true, trim: true },
+//     floor: { type: Number, required: false, trim: true },
+//     flatNumber: { type: Number, required: false, trim: true },
+//     isActive: {type: Boolean, default: true},
+// }, { timestamps: true })
 
-const AddressModel = mongoose.model('Address', AddressSchema);
-export default AddressModel;
+// const AddressModel = mongoose.model('Address', AddressSchema);
+// export default AddressModel;
