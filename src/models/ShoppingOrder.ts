8c65635b1be40588d285d5c1177bd613d87243d5
eub1 <ebonforte@gmail.com>
2022-11-30 21:22:26 -0300

    + user: String, y cambio order por items

--- src/models/ShoppingOrder.ts
+++ src/models/ShoppingOrder.ts
@@ -3,14 +3,15 @@ const { model , Schema } = mongoose;
 
 
 const ShoppingOrderSchema = new Schema({
-    user_id: {type : mongoose.Types.ObjectId , ref: "User" , required : true },
-    order_address : {type: String, trim: true, required : true },
-    order: {type: Object},
-    orderDate : {type : Date , default : Date.now },
-    payment : { type : Object},
-    shippingMethod : {type: String, trim: true },
-    orderTotal : {type : Number, trim: true },
-    orderStatus : { type : Number, trim: true },
+    user_id: {type: mongoose.Types.ObjectId , ref: "User" , required : true },
+    user: {type: String, required : true },
+    order_address: {type: String, trim: true, required : true },
+    items: {type: Object},
+    orderDate: {type: Date , default : Date.now },
+    payment: { type: Object},
+    shippingMethod: {type: String, trim: true },
+    orderTotal: {type: Number, trim: true },
+    orderStatus: { type: Number, trim: true },
 }, { timestamps: true });
 
 const ShoppingOrder = model('ShoppingOrder' , ShoppingOrderSchema)
