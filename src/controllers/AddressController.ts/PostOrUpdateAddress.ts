b66453ea72c77c4fe6780c8bf6d83add467e384a
eub1 <ebonforte@gmail.com>
2022-12-01 19:05:17 -0300

    corrijo errores

--- src/controllers/AddressController.ts/PostOrUpdateAddress.ts
+++ src/controllers/AddressController.ts/PostOrUpdateAddress.ts
@@ -12,20 +12,31 @@ const updateOrPostAddress = async(user: any, address: any)=>{
   console.log("user", user);
   console.log("user.address", user.address);
   console.log("user.address.length",user.address.length);
-
-  if ( user.address.length >= 3 ) throw new Error("can't keep more than 3 addresses, please remove one");
-
-  const checkIndex = user.address.indexOf(address.addressNum)
-  if(checkIndex > 0) {
-    user.address = user.address.splice(checkIndex, 1, {address})
-    const updatedUser= await user.save()
-  } else {
-    user.address = user.address.push({address})
-    const updatedUser= await user.save()
-  }
-  const getUser: any = await User.findById(user._id).select( "address")
   
-  console.log("updatedUser", getUser.address[0]);
+  console.log('address.addressNum = ', address.addressNum);
+  const checkIndex = user.address.indexOf(address.addressNum)
+  // console.log("checkIndex ", checkIndex );
+  
+  if(checkIndex >= 0) {
+    console.log('entre en >= 0');
+    
+    user.address.splice(checkIndex, 1, address)
+    const updatedUser= await user.save()
+    console.log('updated user 1x = ', updatedUser);
+    
+  } else {
+    console.log('entre al else');
+
+    if ( user.address.length >= 3 ) throw new Error("can't keep more than 3 addresses, please remove one");
+    
+    user.address.push(address)
+    const updatedUser= await user.save()
+    console.log('updatedUser 2x = ', updatedUser);
+    
+  }
+  const getUser: any = await User.findById(user._id).select("address")
+  
+  console.log("updatedUser", getUser);
   
   return getUser
 }
