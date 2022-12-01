cc949e11bf4a6fab58975815aac85359bd3be8f3
eub1 <ebonforte@gmail.com>
2022-11-30 21:26:33 -0300

    items es array de objetos

--- src/models/ShoppingOrder.ts
+++ src/models/ShoppingOrder.ts
@@ -6,7 +6,7 @@ const ShoppingOrderSchema = new Schema({
     user_id: {type: mongoose.Types.ObjectId , ref: "User" , required : true },
     user: {type: String, required : true },
     order_address: {type: String, trim: true, required : true },
-    items: {type: Object},
+    items: {type: [Object]},
     orderDate: {type: Date , default : Date.now },
     payment: { type: Object},
     shippingMethod: {type: String, trim: true },
