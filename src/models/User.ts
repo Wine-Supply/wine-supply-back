072748444777789490cded290048208242423bc2
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-12-02 02:59:17 -0300

    arreglo conflicto

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
   userName: {type: String, required: true, minLength: 3, maxLength: 15, trim: true },
   email: {type: String, required: true, unique: true, minLength: 8,  maxLength: 50, trim: true },
   hashedPass:{type: String, required: true},
-  date_of_birth:{type : Date, required: false},
+  date_of_birth:{type : Date, required: true},
   phone: {type: String, minLength: 6, maxLength: 15, trim: true}, //* Los numeros de tel tienen "-" y a veces "()"
   avatar:{type: String, required: false},
   isAdmin: {type: String, default: "no"},
