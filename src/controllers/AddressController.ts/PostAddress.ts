2d6494932722e0e55ccc4c6a38f129abac27b872
eub1 <ebonforte@gmail.com>
2022-12-02 18:20:00 -0300

    optimizo logica de isDefault en address

--- src/controllers/AddressController.ts/PostAddress.ts
+++ src/controllers/AddressController.ts/PostAddress.ts
@@ -6,19 +6,19 @@ const postAddress = async(user: any, address: any, query: any)=>{
 
     if ( user.address.length >= 3 ) throw new Error("can't keep more than 3 addresses, please remove one");
 
-  // si el nuevo address a postear, isDefault es true, entonces debo cambiar a false el que estaba previamente en true
-  // enviar por query el index del que debo cambiar a false.
-  // ejemplo: /address/update?index=0&false=1
+  // ejemplo: /address/update?index=0
+  
+  // si la propiedad isDefault viene como true, entonces todas las anteriores se cambian a false, en el proximo paso, queda registrado el true
+  if(address.isDefault){
 
-    if(query.false){
-      let f = parseInt(query.false) 
-      user.address[f].isDefault = false
-    }
+    user.address.forEach( (el:any) => el.isDefault = false)
+
+  }
 
     user.address.push(address)
     const updatedUser= await user.save()
 
-   console.log("updatedUser", updatedUser);
+  //  console.log("updatedUser", updatedUser);
   
   return updatedUser.address
 }
