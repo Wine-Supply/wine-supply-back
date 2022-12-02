7c4a270bc44e37621b31b466eca670a2932bdb95
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-12-02 14:10:18 -0300

    userchange_front

--- src/routes/authRoutes/GetUser.ts
+++ src/routes/authRoutes/GetUser.ts
@@ -6,17 +6,15 @@ const router = Router()
 
 router.get("/", async(req: any, res, next) => {
     
-    const userId = req.user._id;
-    let user;
+    const user = req.user
     try {
-        user = await User.findById(userId, "-hashedPass")
+        return res.status(200).json({user})
     } catch(error) {
         return res.send("error")
     }
     if (!user) {
         return res.status(400).send("User not found!")
     }
-    return res.status(200).json({user})
 })
 
 //
