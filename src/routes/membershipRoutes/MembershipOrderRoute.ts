fe99db978aaeedb7993819665ff39da8ee783dec
eub1 <ebonforte@gmail.com>
2022-12-03 19:34:55 -0300

    creo ruta post para membership order

--- src/routes/membershipRoutes/MembershipOrderRoute.ts
+++ src/routes/membershipRoutes/MembershipOrderRoute.ts
@@ -1,20 +1,27 @@
 import { Router } from "express";
 const mercadopago = require('mercadopago')
-import ShoppingOder from "../../models/ShoppingOrder"
-import postMembershipOrder from "../../controllers/PaymentsControllers/PostMembershipOrder";
+import postOrder from "../../controllers/PaymentsControllers/PostOrder";
+import postMembership from '../../controllers/Membership controllers/PostMembership'
+
 
 const router = Router()
 
 // *  /membership
 
 router.post("/", async (req, res) => {
+  try {
 
     const response = req.query;
 	  const orderShopId = response.merchant_order_id;
 	  const data = await mercadopago.merchant_orders.findById(orderShopId);
-    postMembershipOrder(response, data)
+    postOrder(response, data)
+    postMembership(response,data)
 	
-	res.send(data);
+	  res.send(data);
+
+  } catch(error: any) {
+    return res.status(400).send({error: error.message})
+  }
 
 })
 
