c847d47d233653efb70c9e74dba4e87a439684e4
eub1 <ebonforte@gmail.com>
2022-12-03 13:32:58 -0300

    update model name: enum, add user_id, isActive

--- src/models/Membeship.ts
+++ src/models/Membeship.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const MembershipSchema = new Schema ({
-    name: { type: String, required: true, trim: true },
+    name: { type: String, required: true, enum:['Regular', 'Premium', 'Sommeliere']},
     price: { type: Number, required: true },
-    isPremium: { type: Boolean, required: true}
+    user_id: {type: mongoose.Types.ObjectId, ref:"User", required: true},
+    isActive: { type: Boolean }
 }, { timestamps: true })
 
 const MembershipModel = mongoose.model('Membership', MembershipSchema);
