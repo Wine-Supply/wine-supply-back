a347eede884cf41cc00156c511788c0602cefb26
eub1 <ebonforte@gmail.com>
2022-12-05 01:12:29 -0300

    agrego user que viene de verifyUserToken

--- src/routes/membershipRoutes/MembershipOrderRoute.ts
+++ src/routes/membershipRoutes/MembershipOrderRoute.ts
@@ -8,16 +8,20 @@ const router = Router()
 
 // *  /membership
 
-router.post("/", async (req, res) => {
+router.post("/", async (req:any, res:any) => {
   try {
 
+    const user = req.user;
+		//console.log("user", user);
+		if (!user.isActive) {return res.status(303).send("Inactive user, do you want to recover it?") };
+		if (!user) { return res.status(404).send("User not found!") };
     const response = req.query;
-	  const orderShopId = response.merchant_order_id;
-	  const data = await mercadopago.merchant_orders.findById(orderShopId);
-    postOrder(response, data)
-    postMembership(response,data)
+	  // const orderShopId = response.merchant_order_id;
+	  // const data = await mercadopago.merchant_orders.findById(orderShopId);
+    // postOrder(response, data);
+    const newMembership = postMembership(response, user);
 	
-	  res.send(data);
+	  res.send(newMembership);
 
   } catch(error: any) {
     return res.status(400).send({error: error.message})
