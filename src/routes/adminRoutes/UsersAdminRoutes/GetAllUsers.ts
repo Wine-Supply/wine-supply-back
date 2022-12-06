c5104fb658dcc945cc63b269921066a24f1bcc30
eub1 <ebonforte@gmail.com>
2022-12-06 15:17:53 -0300

    agrego condiciones de query y response

--- src/routes/adminRoutes/UsersAdminRoutes/GetAllUsers.ts
+++ src/routes/adminRoutes/UsersAdminRoutes/GetAllUsers.ts
@@ -3,33 +3,45 @@ import User from "../../../models/User"
 const router = Router()
 
 //* /admin/users
-// verify Token
+// verify Token AdminStatus
 
 router.get("/", async(req: any, res) => {
 
-  // trae a todos los usuarios (no trae a los usuarios admin)
+  // si llega por query newUser=true, devuelve los ultimos 5 usuarios creados activos, ordenados por id
+  // si llega por query isActive=yes, devuelve solo los usuarios activos, ordenados por _id
+  // si llega por query isActive=no, devuelve solo los usuarios inactivos, ordenados por _id
+  // si llega por query isAdmin=true, devuelve solo los usuarios con rol de admin, ordenados por _id
+  // si llega por query all=true, devuelve todos los usuarios (no trae a los usuarios admin)
 
-  // si llega por query new=true, devuelve los ultimos 5 usuarios creados
-  // si llega por query isActive=true, devuelve solo los usuarios activos
-  // si llega por query isActive=false, devuelve solo los usuarios inactivos
-  // si llega por query isAdmin=true, devuelve solo los usuarios con rol de admin
-
-  const newUsers = req.query.new;
-  const activeUsers = req.query.isActive
-  const adminUsers = req.query.isAdmin
+  const {newUser, isActive, isAdmin, all} = req.query;
 
   try {
 
-    const users:any = newUsers ? await User.find({isAdmin: "no", isActive: true}).sort({_id: -1}).limit(5) : await User.find();
-        
-      return res.status(200).send(users)
+    if(newUser) {
+      const newUsers = await User.find({isAdmin: "no", isActive: true}).sort({_id: -1}).limit(5)
+      return res.status(200).send(newUsers)
+    }
+    if (isAdmin){
+      const adminUsers = await User.find({isAdmin: "yes"}).sort({_id: -1})
+      return res.status(200).send(adminUsers)
+    }
+    if (isActive==="yes") {
+      const active = await User.find({isAdmin: "no", isActive: true}).sort({_id: -1})
+      return res.status(200).send(active)
+    }
+    if (all) {
+      const allUsers = await User.find()
+      return res.status(200).send(allUsers)
+    }
+    if (isActive ==="no") {
+      const inactive = await User.find({isAdmin: false, isActive: false}).sort({_id: -1})
+      return res.status(200).send(inactive)
+    }
       
     } catch(error:any) {
         return res.send(error.message)
     }
 })
-
-//
 
 
 export default router
