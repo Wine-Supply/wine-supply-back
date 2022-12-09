6a12f1f7cd0efcd3457c965dbf455b5a253cf212
Franco Nicoletti <franconicoletti.14@gmail.com>
2022-12-09 15:48:48 -0300

    react admin routes

---
+++ src/routes/adminRoutes/WinesReactAdmin/GetWinesAdmin.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import getAllWinesAdmin from "../../../controllers/ReactAdminControllers/GeatAllWinesAdmin";
+
+const router = Router()
+
+//* /admin/wines
+
+
+router.get("/", async (req, res) => {
+
+  try {
+    const wines = await getAllWinesAdmin();
+    let total = wines.length
+    res.header({'content-Range':  `wines 0-20/${total}`, 'Access-Control-Expose-Headers': 'Content-Range'})
+    return res.status(200).send(wines);
+  } 
+  catch (error: any) {
+    return res.status(404).send('Cant get wines!')
+  }
+
+});
+
+
+
+export default router;
