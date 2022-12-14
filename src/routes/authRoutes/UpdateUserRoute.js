9b944e3a1a03945d3168d14e9443a543c83d2b8c
FranSabt <hsabatino1@gmail.com>
2022-12-14 13:29:31 -0400

    mailPay

--- src/routes/authRoutes/UpdateUserRoute.js
+++ src/routes/authRoutes/UpdateUserRoute.js
@@ -12,8 +12,10 @@ router.put("/", async (req, res) => {
 	try {
 		const user = req.user;
 		//console.log("user", user);
+		if (!user) {
+			return res.status(404).send("User not found!");
+		}
 		if (!user.isActive) {
-			if (req.body.isActive) {
 				const updatedUser = await updateUser(user, req.body);
 
 				return res.status(200).send(updatedUser);
@@ -21,38 +23,36 @@ router.put("/", async (req, res) => {
 				return res
 					.status(303)
 					.send("Inactive user, do you want to recover it?");
-		}
+				}
+				catch (error) {
+				   return res.status(400).send({ error: error.message });
+			   }
 
-		if (!user) {
-			return res.status(404).send("User not found!");
-		}
 
-		if (Object.keys(req.body).length) {
-			//!!Update Avatar
-			if (req.files?.image && avatarID) {
-				//* Preguta si hay imagen y ID para el procedimiento
-				const destroy = await destroyImg(avatarID); //* Elimina imagen anterior
-				result = await upLoadImg(req.files.images.tempFilePath, "Users"); //* Guarda imagen folder "Users" y devuelve info
-				await fs.unlink(req.files.images.tempFilePath); //* Elimina imagen del servidor de Node.js
+		// if (Object.keys(req.body).length) {
+		// 	//!!Update Avatar
+		// 	if (req.files?.image && avatarID) {
+		// 		//* Preguta si hay imagen y ID para el procedimiento
+		// 		const destroy = await destroyImg(avatarID); //* Elimina imagen anterior
+		// 		result = await upLoadImg(req.files.images.tempFilePath, "Users"); //* Guarda imagen folder "Users" y devuelve info
+		// 		await fs.unlink(req.files.images.tempFilePath); //* Elimina imagen del servidor de Node.js
 
-				req.body.avatar = [
-					//* Coloca rutas en la propieda imagen
-					result.secure_url, //Direccion de la imagen
-					result.public_id //Id de la imagen
-				];
-			} else {
-				console.log("Image or old-avatarID not provided");
-			}
-			//!!Update Image END
+		// 		req.body.avatar = [
+		// 			//* Coloca rutas en la propieda imagen
+		// 			result.secure_url, //Direccion de la imagen
+		// 			result.public_id //Id de la imagen
+		// 		];
+		// 	} else {
+		// 		console.log("Image or old-avatarID not provided");
+		// 	}
+		// 	//!!Update Image END
 
-			const updatedUser = await updateUser(user, req.body);
+		// 	const updatedUser = await updateUser(user, req.body);
 
-			return res.status(200).send(updatedUser);
-		}
-		return res.status(400).send({ error: "No parameters sent for update" });
-	} catch (error) {
-		return res.status(400).send({message: "Can not update :c", error: error.message });
-	}
+		// 	return res.status(200).send(updatedUser);
+		// }
+		// return res.status(400).send({ error: "No parameters sent for update" });
 });
 
 export default router;
+
